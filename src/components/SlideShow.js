d2982b01c3628b632d3225da382a128ee9d480ae
Joshua Hewitson <j32hewitson@gmail.com>
2019-12-04 14:39:35 +0200

    updated screenshots

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -3,17 +3,17 @@ import Slider from 'react-animated-slider'
 import 'react-animated-slider/build/horizontal.css'
 import Grid from '@material-ui/core/Grid'
 import phoneBackground from '../assets/phoneBackground.svg'
-import slide1 from '../assets/phoneOutline3.svg'
-import slide2 from '../assets/phoneOutline4.svg'
-import slide3 from '../assets/phoneOutline6.svg'
-import slide4 from '../assets/phoneOutline7.svg'
+import slide1 from '../assets/screen_shot_1.png'
+import slide2 from '../assets/screen_shot_2.png'
+import slide3 from '../assets/screen_shot_3.png'
+import slide4 from '../assets/screen_shot_4.png'
 import '../App.css'
 
 const slides = [
-  <img key={0} class='slideAppear' style={{ position: 'absolute', top: 10, left: 0, zIndex: 2 }} src={slide1} />,
-  <img key={1} class='slideAppear' style={{ position: 'absolute', top: 10, left: 0, zIndex: 2 }} src={slide2} />,
-  <img key={2} class='slideAppear' style={{ position: 'absolute', top: 10, left: 0, zIndex: 2 }} src={slide3} />,
-  <img key={3} class='slideAppear' style={{ position: 'absolute', top: 10, left: 0, zIndex: 2 }} src={slide4} />
+  <img key={0} class='slideAppear' alt='screen 1' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }} src={slide1} />,
+  <img key={1} class='slideAppear' alt='screen 2' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }} src={slide2} />,
+  <img key={2} class='slideAppear' alt='screen 3' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }} src={slide3} />,
+  <img key={3} class='slideAppear' alt='screen 4' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }} src={slide4} />
 ]
 
 class SlideShow extends PureComponent {
@@ -40,9 +40,9 @@ class SlideShow extends PureComponent {
             overflow: 'hidden',
             paddingTop: '120%',
             width: '57%',
-            // border: 'solid rgb(30, 20, 25) 4px',
+            // border: 'solid rgb(154, 164, 183) 3px',
             boxShadow: '7px 7px 20px 10px rgba(47,137,128, 0.2)',
-            borderRadius: 25,
+            borderRadius: 30,
             backgroundColor: 'white',
             position: 'relative',
             top: 0,
